ea50b4b2150305baa9ee59c6d16543886611f49f
Preeyapong <tos2013s@gmail.com>
2020-04-22 17:20:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<h1>Welcome Heroku test</h1>
-</body>
-</html>
+<?php
+
+
+$API_URL = 'https://api.line.me/v2/bot/message';
+$ACCESS_TOKEN = 'fKBQVzkXsQjHfqDMCSBD2n1dFDKM6qCKsgpXEB+28TySJPM47SpZpieoKL8dxZpL9ylvJ3PjPQ/KNTNzLHUjoqjUkl6pf2zQjRZwcYExZPRA57+iTcc+rHfADMilDRxodUJWpqI3nHTGjg/rdhJLRAdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '53c9645feff2606a53f2f416fc1fb12e';
+
+
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+
+
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+       
+       $reply_message = '';
+       $reply_token = $event['replyToken'];
+       $text = $event['message']['text'];
+       $data = [
+          'replyToken' => $reply_token,
+          'messages' => [['type' => 'text', 'text' => $text ]]
+       ];
+       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+       $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+       echo "Result: ".$send_result."\r\n";
+     }
+ }
+
+echo "OK";
+
+
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+?>
