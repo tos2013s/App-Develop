8dd46a5ef9c54152b5fc122e43fd4827ec4ee86a
Preeyapong <tos2013s@gmail.com>
2021-02-17 22:38:54 +0700

    Update lineAlert-01.php

--- Tos_test/lineAlert-01.php
+++ Tos_test/lineAlert-01.php
@@ -9,8 +9,26 @@
 	$sMessage .="swan industries\r\n";
 	//$sMessage .= askForRequestedArguments();
 
+function parseHeaders( $headers )
+{
+    $head = array();
+    foreach( $headers as $k=>$v )
+    {
+        $t = explode( ':', $v, 2 );
+        if( isset( $t[1] ) )
+            $head[ trim($t[0]) ] = trim( $t[1] );
+        else
+        {
+            $head[] = $v;
+            if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                $head['reponse_code'] = intval($out[1]);
+        }
+    }
+    return $head;
+}
 
-	$sMessage .= print_r($_POST);
+
+	$sMessage .= print_r(parseHeaders($http_response_header));
 	
 	$chOne = curl_init(); 
 	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
