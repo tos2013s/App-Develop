400593cefc3a5b0d22089a9b4765225d34de854d
Preeyapong <tos2013s@gmail.com>
2021-02-17 16:10:05 +0700

    Update line-notify-01.php

--- line-notify-01.php
+++ line-notify-01.php
@@ -1,47 +1,34 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header("Content-Type: text/plain");
 
-define('LINE_API',"https://notify-api.line.me/api/notify");
- 
-$token ="4dsVfCgR7LpqIGOooWHVYSzVYCg0QSAhtqy8mcdBOwV"; // develop for test
-$sMessage .="\r\nswan industries\r\n";
-$sMessage ="subject\r\n";
-$WebParameter = null;
+$accToken = "UwJJvP1lFFwR4AIOg89RaeMMy440L8UbioXZ6c1QJu1";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$WebParameter .= $_POST['message'];
-}
-else if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-	$WebParameter .= $_GET['message'];	
-}
-else{
-	$WebParameter .= 'no REQUEST_METHOD..GET';
-}
+$jsonALL = file_get_contents("https://covid19.th-stat.com/api/open/today");
+$jsonz = preg_replace('/\r|\n/','\n',trim($jsonALL));
+$jsonData = json_decode($jsonz, TRUE);
 
+$sMessage .=json_encode($jsonData, JSON_PRETTY_PRINT);
 
-$sMessage .= "\r\n".$WebParameter;
-$res = notify_message($sMessage,$token);
-print_r($res);
+	$chOne = curl_init(); 
+	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+	curl_setopt( $chOne, CURLOPT_POST, 1); 
+	curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
+	$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$accToken.'', );
+	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+	$result = curl_exec( $chOne ); 
 
-function notify_message($message,$token){
- $queryData = array('message' => $message);
- $queryData = http_build_query($queryData,'','&');
- $headerOptions = array( 
-         'http'=>array(
-            'method'=>'POST',
-            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
-                      ."Authorization: Bearer ".$token."\r\n"
-                      ."Content-Length: ".strlen($queryData)."\r\n",
-            'content' => $queryData
-         ),
- );
- $context = stream_context_create($headerOptions);
- $result = file_get_contents(LINE_API,FALSE,$context);
- $res = json_decode($result);
- return $res;
-}
+	//Result error 
+	if(curl_error($chOne)) 
+	{ 
+		echo 'error:' . curl_error($chOne); 
+	} 
+	else { 
+		//$result_ = json_decode($result, true); 
+		//echo "status : ".$result_['status']; echo "message : ". $result_['message'];
+		echo "meassage ok.";
+	} 
+	curl_close( $chOne ); 
 
 ?>
