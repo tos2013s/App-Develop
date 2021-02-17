355dae0f0447ef6ed0eafbc933ecdccb3bc07226
Preeyapong <tos2013s@gmail.com>
2021-02-17 22:57:29 +0700

    Create lineAlert-02.php

---
+++ Tos_test/lineAlert-02.php
@@ -0,0 +1,40 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	date_default_timezone_set("Asia/Bangkok");
+
+	$sToken = "Xc5gLo8IFM4b1t5GQII2oakRvEzxveFzsVUBr7iYrjj"; // develop alone
+	$sMessage .="swan industries\r\n";
+	//$sMessage .= askForRequestedArguments();
+
+
+$rawdata = $HTTP_RAW_POST_DATA;
+// Or maybe we get XML
+$decoded = simplexml_load_string($rawdata);
+$sMessage .= $decoded ;
+	
+	$chOne = curl_init(); 
+	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+	curl_setopt( $chOne, CURLOPT_POST, 1); 
+	curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
+	$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
+	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+	$result = curl_exec( $chOne ); 
+
+	//Result error 
+	if(curl_error($chOne)) 
+	{ 
+		echo 'error:' . curl_error($chOne); 
+	} 
+	else { 
+		//$result_ = json_decode($result, true); 
+		//echo "status : ".$result_['status']; echo "message : ". $result_['message'];
+		echo "meassage ok.";
+	} 
+	curl_close( $chOne );
+
+?>
